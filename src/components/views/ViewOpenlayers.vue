56d42553128ff89900bd4056ec6d9322f30b157b
Claire Monney <c.monney@master.hes-so.ch>
2020-12-06 18:10:40 +0100

    modification geoloc et lecture json

--- src/components/views/ViewOpenlayers.vue
+++ src/components/views/ViewOpenlayers.vue
@@ -50,6 +50,10 @@ import Geolocation from 'ol/Geolocation';
 import VectorLayer from 'ol/layer/Vector';
 import VectorSource from 'ol/source/Vector';
 import GeoJSON from 'ol/format/GeoJSON';
+import {Fill, Stroke, Circle, Style} from 'ol/style';
+import Point from 'ol/geom/Point';
+import Feature from 'ol/Feature';
+
 
 import {watchposition} from '../../services/geolocation.js'
 //import {changeImageMap} from '../../services/changeimage.js'
@@ -77,6 +81,7 @@ export default {
         maximumAge        : 30000, 
         timeout           : 27000
       }, 
+      position: null,
     }
 
 
@@ -126,11 +131,12 @@ export default {
       }
     },
 
-// méthodes pour obtenir la géolocalisation 
-
-
+    // méthodes pour obtenir la géolocalisation 
     marker(position){
-      console.log(position.timestamp, position.coords.latitude, position.coords.longitude); 
+      console.log(position.coords.accuracy, position.timestamp, position.coords.latitude, position.coords.longitude);
+      var coordinates = [position.coords.latitude, position.coords.longitude];
+      return coordinates
+
     },
 
     message(message){
@@ -141,19 +147,39 @@ export default {
       watchposition(callback, errorcallback, geooptions)
     }, 
 
+    posPoint(coordinates, map){
+      var positionFeature = new Feature();
+      positionFeature.setGeometry(coordinates ?
+        new Point(coordinates): null);
 
+      new VectorLayer({
+        map: map,
+        source: new VectorSource({
+          features:[positionFeature]
+        })
+      });
+    },
+
+    //import GeoJSON
+    readFeature(features){
+      //var myfeature = features.item(0);
+      var code = features.get('CAT_CODE');
+      console.log(code);
+    },
 
     importGeoJSON (){
-      var json = new VectorLayer({
+      var geojson = new VectorLayer({
         source: new VectorSource({
           projection : 'EPSG:3857',
           url: 'donnees_rando_3857_def.geojson',
           format: new GeoJSON(),
         })
       })
-      console.log(json)
-      this.olmap.addLayer(json)
-      console.log(this.olmap)
+      //console.log(new GeoJSON().readFeatures('../../assets/data/donnees_rando_3857_def.geojson'))
+      //console.log(geojson.parse)
+      //this.olmap.addLayer(geojson)
+      //this.readFeature(geojson)
+      //console.log(this.olmap)
     },
 
 
@@ -166,10 +192,12 @@ export default {
     this.olmap.addLayer(this.layersOSM);
     //this.olmap.getvisible(OSM);
 
-    this.localisation(this.marker, this.message, this.geo_options);
+
+    //this.localisation(this.marker, this.message, this.geo_options);
+    this.posPoint(this.localisation(this.marker, this.message, this.geo_options), this.olmap)
 
     //this.olmap.addLayer(
-    //this.importGeoJSON();
+    this.importGeoJSON();
     //this.content = JSON.parse(importGeoJSON);
 
 
