a2477d7f94bc9c98e7424ec946e4ff6e0a8f1828
claire-master <73231201+claire-master@users.noreply.github.com>
2021-01-03 17:17:59 +0100

    ajout route rapport

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,10 @@
 import { createWebHistory, createRouter } from "vue-router";
 import VhelloWorld from "@/components/views/ViewHome.vue";
-import Vleaflet from "@/components/views/ViewLeaflet.vue";
+// import Vleaflet from "@/components/views/ViewLeaflet.vue";
 import Vcesium from "@/components/views/ViewCesium.vue";
 import Vaxios from "@/components/views/ViewAxios.vue";
 import Vopenlayers from "@/components/views/ViewOpenlayers.vue";
+// import Vrapport from "@/components/views/ViewRapport.vue";
 import NotFound404 from "@/components/errors/404page.vue";
 
 const routes = [
@@ -13,25 +14,31 @@ const routes = [
     component: VhelloWorld,
   },
   {
-    path: "/axios",
+    path: "/recherche",
     name: "Axios",
     component: Vaxios,
   },
   {
-    path: "/openlayers",
+    path: "/carte",
     name: "Openlayers",
+    props: true,
     component: Vopenlayers,
   },
+  // {
+  //   path: "/leaflet",
+  //   name: "Leaflet",
+  //   component: Vleaflet,
+  // },
   {
-    path: "/leaflet",
-    name: "Leaflet",
-    component: Vleaflet,
-  },
-  {
-    path: "/cesium",
+    path: "/3d",
     name: "Cesium",
     component: Vcesium,
   },
+  // {
+  //   path: "/rapport",
+  //   name: "rapport",
+  //   component: Vrapport,
+  // },
   {
     path: "/:catchAll(.*)",
     name: "404 page",
